5322fc4531a6cab17a0f0e5d98e521ad94da9af8
Dan Berglund <dan@berglund.io>
2021-12-17 15:32:18 +0100

    Use a config-object instead of mulitple GET-parameters

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,51 @@
 extern crate rss;
+extern crate serde;
 extern crate serde_json;
 extern crate handlebars;
 extern crate reqwest;
 
+use serde::Deserialize;
 use serde_json::Value;
 use rss::{ChannelBuilder, ItemBuilder, Item, EnclosureBuilder, GuidBuilder};
 use handlebars::Handlebars;
 
-pub fn generate_channel<R: std::io::Read>(reader: R, link: &str, title: &str, item_key: &str, url_template: &str, title_template: &str, description_template: &str, image_key: &Option<String>) -> Result<String, ParseError> {
-    let parsed: Value = serde_json::from_reader(reader)?;
-    let items = &parsed[item_key].as_array().ok_or(ParseError::ItemArrayIncorrect)?;
-    let rss_items: Result<Vec<_>, _> = items.into_iter().map(|item| create_item(item, url_template, title_template, description_template, image_key)).collect();
+#[derive(Deserialize)]
+pub struct Config {
+    source: String,
+    link: String,
+    title: String,
+    item_key: String,
+    url_template: String,
+    title_template: String,
+    description_template: String,
+    image_key: Option<String>
+}
+
+pub fn generate_channel(config: &Config) -> Result<String, Error> {
+    let res = reqwest::blocking::get(&config.source)?;
+    let parsed: Value = serde_json::from_reader(res)?;
+    let items = &parsed[&config.item_key].as_array().ok_or(Error::ItemArrayIncorrect)?;
+    let rss_items: Result<Vec<_>, _> = items.into_iter().map(|item| create_item(item, config)).collect();
     let valid_items: Vec<_> = rss_items?;
     let channel = ChannelBuilder::default()
-        .title(title.to_string())
-        .link(link.to_string())
+        .title(&config.title)
+        .link(&config.link)
         .items(valid_items)
         .build();
     return Ok(channel.to_string());
 }
 
-pub fn create_item(item: &Value, url_template: &str, title_template: &str, description_template: &str, image_key: &Option<String>) -> Result<Item, ParseError> {
-    let url = render(url_template, item)?;
-    let title = render(title_template, item)?;
-    let description = render(description_template, item)?;
+pub fn create_item(item: &Value, config: &Config) -> Result<Item, Error> {
+    let url = render(&config.url_template, item)?;
+    let title = render(&config.title_template, item)?;
+    let description = render(&config.description_template, item)?;
     let mut rss_item = ItemBuilder::default()
         .guid(GuidBuilder::default().value(url).build())
         .title(title)
         .description(description)
         .build();
-    if let Some(image_key) = image_key {
-        let image_url = item[image_key].as_str().ok_or(ParseError::CouldNotParseImage)?;
+    if let Some(image_key) = &config.image_key {
+        let image_url = item[image_key].as_str().ok_or(Error::CouldNotParseImage)?;
         let mut image = EnclosureBuilder::default()
             .url(image_url)
             .mime_type("image/jpeg")
@@ -43,37 +58,44 @@ pub fn create_item(item: &Value, url_template: &str, title_template: &str, descr
     return Ok(rss_item);
 }
 
-fn render(template_str: &str, item: &Value) -> Result<String, ParseError> {
+fn render(template_str: &str, item: &Value) -> Result<String, Error> {
     let handlebars = Handlebars::new();
     let res = handlebars.render_template(template_str, item);
     return Ok(res?);
 }
 
 #[derive(Debug)]
-pub enum ParseError {
+pub enum Error {
     ItemArrayIncorrect,
     TemplateError(handlebars::TemplateError),
     RenderError(handlebars::RenderError),
     CouldNotParseId,
     CouldNotParseImage,
-    JSONError(serde_json::Error)
+    JSONError(serde_json::Error),
+    FetchError(reqwest::Error)
 }
 
-impl From<serde_json::Error> for ParseError {
+impl From<serde_json::Error> for Error {
     fn from(serde_err: serde_json::Error) -> Self {
         return Self::JSONError(serde_err);
     }
 }
 
-impl From<handlebars::TemplateError> for ParseError {
+impl From<handlebars::TemplateError> for Error {
     fn from(err: handlebars::TemplateError) -> Self {
         return Self::TemplateError(err);
     }
 }
 
-impl From<handlebars::RenderError> for ParseError {
+impl From<handlebars::RenderError> for Error {
     fn from(err: handlebars::RenderError) -> Self {
         return Self::RenderError(err);
+    }
+}
+
+impl From<reqwest::Error> for Error {
+    fn from(err: reqwest::Error) -> Self {
+        return Self::FetchError(err);
     }
 }
 
@@ -84,7 +106,17 @@ mod tests {
     #[test]
     fn care_by_volvo() {
         let file = std::fs::File::open("testdata/CbV.json").unwrap();
-        let res = generate_channel(&file, "https://www.volvocars.com/se/care-by-volvo/cars/", "Care by Volvo", "data", "https://www.volvocars.com/se/care-by-volvo/cars/{{vehicleId}}/", "{{title}} ({{engineType}})", "{{basePrice}}:-/Mån\n{{engineDescription}}\n{{#each environmentalDataDetails.wltp}}{{this.label}}: {{this.value}}\n{{/each}}\n\nUppskattad leverans: {{estimateDeliveryDate}}", &Some("image".to_string()));
+        let config = Config {
+            source: "https://www.volvocars.com/api/care-by-volvo/cars/cars/?customerType=b2c&filters.delivery=stock&itemsPerPage=18&market=se&page=1".to_string(),
+            link: "https://www.volvocars.com/se/care-by-volvo/cars/".to_string(),
+            title: "Care by Volvo".to_string(),
+            item_key: "data".to_string(),
+            url_template: "https://www.volvocars.com/se/care-by-volvo/cars/{{vehicleId}}/".to_string(),
+            title_template: "{{title}} ({{engineType}})".to_string(),
+            description_template: "{{basePrice}}:-/Mån\n{{engineDescription}}\n{{#each environmentalDataDetails.wltp}}{{this.label}}: {{this.value}}\n{{/each}}\n\nUppskattad leverans: {{estimateDeliveryDate}}".to_string(),
+            image_key: Some("image".to_string())
+        };
+        let res = generate_channel(&config);
         println!("{:?}", res);
         assert!(res.is_ok());
     }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate rouille;
 extern crate rss;
+extern crate serde;
 extern crate serde_json;
 extern crate reqwest;
 extern crate handlebars;
@@ -26,33 +27,26 @@ fn main() {
 }
 
 fn process_request(request: &Request) -> Result<String, Error> {
-    let source_url = request.get_param("source").ok_or(Error::SourceURLMissing)?;
-    let link = request.get_param("link").ok_or(Error::LinkMissing)?;
-    let title = request.get_param("title").ok_or(Error::TitleMissing)?;
-    let items_key = request.get_param("items_key").ok_or(Error::ItemsKeyMissing)?;
-    let url_template = request.get_param("url_template").ok_or(Error::UrlTemplateMissing)?;
-    let title_template = request.get_param("title_template").ok_or(Error::TitleTemplateMissing)?;
-    let description_template = request.get_param("description_template").ok_or(Error::DescriptionTemplateMissing)?;
-    let image_key = request.get_param("image_key");
-    let res = reqwest::blocking::get(source_url)?;
-    return lib::generate_channel(res, &link, &title, &items_key, &url_template, &title_template, &description_template, &image_key).map_err(|err| Error::ParseError(err));
+    let config_str = request.get_param("config").ok_or(Error::ConfigMissing)?;
+    let config = serde_json::from_str(&config_str)?;
+    return Ok(lib::generate_channel(&config)?);
 }
 
 #[derive(Debug)]
 pub enum Error {
-    SourceURLMissing,
-    LinkMissing,
-    TitleMissing,
-    ItemsKeyMissing,
-    UrlTemplateMissing,
-    TitleTemplateMissing,
-    DescriptionTemplateMissing,
-    FetchError(reqwest::Error),
-    ParseError(lib::ParseError),
+    ConfigMissing,
+    ConfigParseError(serde_json::Error),
+    GeneratorError(lib::Error)
 }
 
-impl From<reqwest::Error> for Error {
-    fn from(err: reqwest::Error) -> Self {
-        return Self::FetchError(err);
+impl From<serde_json::Error> for Error {
+    fn from(err: serde_json::Error) -> Self {
+        Self::ConfigParseError(err)
+    }
+}
+
+impl From<lib::Error> for Error {
+    fn from(err: lib::Error) -> Self {
+        Self::GeneratorError(err)
     }
 }
